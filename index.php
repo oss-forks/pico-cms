c764a1bcaa532cecc928962636cebf88f6b80e9f
Nikolay Zapolnov <zapolnov@gmail.com>
2014-04-22 00:24:31 +0400

    Allow to overwrite content dir.

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 
 define('ROOT_DIR', realpath(dirname(__FILE__)) .'/');
-define('CONTENT_DIR', ROOT_DIR .'content/');
+
+$content_dir = ROOT_DIR .'content/';
+if (@file_exists(dirname(__FILE__)."/../../wiki/config2.php"))
+	include(dirname(__FILE__)."/../../wiki/config2.php");
+
+define('CONTENT_DIR', $content_dir);
 define('CONTENT_EXT', '.md');
 define('LIB_DIR', ROOT_DIR .'lib/');
 define('PLUGINS_DIR', ROOT_DIR .'plugins/');
